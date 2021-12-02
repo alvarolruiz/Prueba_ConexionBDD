9c9cc036b159d380404c042e8e51b5a990b76cb4
ÁLVARO LAURIÑO RUIZ <alvarolaurino@gmail.com>
2021-12-02 14:36:44 +0100

    fich properties

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,14 @@ import java.util.Properties;
 public class Main {
     static final String NOMBRE_FICHERO_PROPIEDADES = "config.properties";
     private static final String CONSULTA_TABLA_CLIENTES = "SELECT * FROM Personas";
-    static Properties propiedadesConexion = new Properties();
-    static OutputStream os = null;
+    private static Properties propiedadesConexion=null;
     static String DRIVER = "com.mysql.jdbc.Driver";
-    static String URL = "jdbc:mysql://localhost";
+    static String URL = "jdbc:sqlserver://localhost:51916   ";
     static String USUARIO = "alvarolruiz";
     static String CLAVE = "mitesoro99";
 
     public static void main(String[] args) {
-        // write your code here
+
         ResultSet resultado;
         Statement query = null;
         Connection c;
@@ -60,28 +59,34 @@ public class Main {
 
     }
 
-    public static void generarFicheroConfig() {
-        propiedadesConexion.setProperty("driver", DRIVER);
-        propiedadesConexion.setProperty("Url", URL);
-        propiedadesConexion.setProperty("Usuario", USUARIO);
-        propiedadesConexion.setProperty("Contraseña", CLAVE);
+    public void generarFicheroConfig() {
+        propiedadesConexion = setProperties();
+        FileOutputStream fos= null;
         try {
-            os = new FileOutputStream(NOMBRE_FICHERO_PROPIEDADES);
-            propiedadesConexion.store(os, null);
+            fos = new FileOutputStream(NOMBRE_FICHERO_PROPIEDADES);
+            propiedadesConexion.store(fos, null);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-        if (os != null) {
+        if (fos != null) {
             try {
-                os.close();
+                fos.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
+    public Properties setProperties(){
+        Properties propiedadesConexion = new Properties();
+        propiedadesConexion.setProperty("driver", DRIVER);
+        propiedadesConexion.setProperty("Url", URL);
+        propiedadesConexion.setProperty("Usuario", USUARIO);
+        propiedadesConexion.setProperty("Contraseña", CLAVE);
+        return propiedadesConexion;
+    }
 }
 
 
