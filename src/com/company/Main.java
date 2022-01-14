e3d4e040035d5f706386fea92291233f4cccea59
ÁLVARO LAURIÑO RUIZ <alvarolaurino@gmail.com>
2022-01-14 10:18:53 +0100

    fich properties

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,32 +7,27 @@ import java.util.Properties;
 public class Main {
     static final String NOMBRE_FICHERO_PROPIEDADES = "config.properties";
     private static final String CONSULTA_TABLA_CLIENTES = "SELECT * FROM Personas";
-    private static Properties propiedadesConexion=null;
+    private static Properties propiedadesConexion= new Properties();
+    private static Connection c;
+    private static Statement query = null;
+    private static ResultSet resultado;
     static String DRIVER = "com.mysql.jdbc.Driver";
-    static String URL = "jdbc:sqlserver://localhost:51916";
+    static String URL = "jdbc:sqlserver://localhost:51916;database=ConexionBDD";
     static String USUARIO = "alvarolruiz";
     static String CLAVE = "mitesoro99";
 
     public static void main(String[] args) {
 
-        ResultSet resultado;
-        Statement query = null;
-        Connection c;
 
         if (!new File(NOMBRE_FICHERO_PROPIEDADES).exists()) {
             generarFicheroConfig();
         } else {
-            try {
-                propiedadesConexion.load(new FileInputStream(NOMBRE_FICHERO_PROPIEDADES));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            cargarFicheroConfig();
         }
+        c= establecerConexion();
 
         try {
-            c= establecerConexion();
-            query = c.createStatement();
-            resultado=query.executeQuery(CONSULTA_TABLA_CLIENTES);
+
             int i=1;
             while(resultado.next()){
                 System.out.println("["+(i++)+"]");
@@ -46,11 +41,38 @@ public class Main {
 
     }
 
+    private static void selectPersonas() throws SQLException {
+        query = c.createStatement();
+        resultado=query.executeQuery(CONSULTA_TABLA_CLIENTES);
+        int count = 0;
+
+        while (resultado.next()){
+            int id = resultado.getInt("IDPersona");
+            String nombre = resultado.getString(2);
+            String apellido = resultado.getString("apellidosPersona");
+            Date fecha = resultado.getDate("fechaNacimiento");
+            int telefono = resultado.getInt("telefono");
+            String direccion= resultado.getString("direccion");
+            int IDDepartamento = resultado.getInt("IDDepartamento");
+
+            String output = "User #%d: %s - %s - %s - %s";
+            System.out.println(String.format(output, ++count, name, pass, fullname, email));
+        }
+    }
+
+    private static void cargarFicheroConfig() {
+        try {
+            propiedadesConexion.load(new FileInputStream(new File(NOMBRE_FICHERO_PROPIEDADES)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static Connection establecerConexion() {
         Connection c = null;
         try {
             c = DriverManager.getConnection(propiedadesConexion.getProperty("Url"),
-                    propiedadesConexion.getProperty("Usuario"), propiedadesConexion.getProperty("Contraseña"));
+                    propiedadesConexion.getProperty("Usuario"), propiedadesConexion.getProperty("Clave"));
         } catch (SQLException e) {
             e.printStackTrace();
 
@@ -60,7 +82,11 @@ public class Main {
     }
 
     public static void generarFicheroConfig() {
-        propiedadesConexion = setProperties();
+        setProperties();
+        saveProperties();
+    }
+
+    private static void saveProperties() {
         FileOutputStream fos= null;
         try {
             fos = new FileOutputStream(NOMBRE_FICHERO_PROPIEDADES);
@@ -79,13 +105,12 @@ public class Main {
         }
     }
 
-    public static Properties setProperties(){
-        Properties propiedadesConexion = new Properties();
+    public static void setProperties(){
+        propiedadesConexion = new Properties();
         propiedadesConexion.setProperty("driver", DRIVER);
         propiedadesConexion.setProperty("Url", URL);
         propiedadesConexion.setProperty("Usuario", USUARIO);
-        propiedadesConexion.setProperty("Contraseña", CLAVE);
-        return propiedadesConexion;
+        propiedadesConexion.setProperty("Clave", CLAVE);
     }
 }
 
